69191fba27ab08876e8858927772fad6eb13609a
lavinhoque33 <caspianprince33@yahoo.com>
2023-06-22 11:02:49 -0400

    Initial and final

---
+++ ForecastApp/Config/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForecastApp.Config
+{
+    public static class Constants
+    {
+        public const string OPEN_WEATHER_APPID = "2d716452512427c5fd5bbfe5262ea00f";
+    }
+}
